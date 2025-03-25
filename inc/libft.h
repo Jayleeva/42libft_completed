04f3e306f1c06df287c9b58a03a11b0e159fc243
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2025-03-25 10:58:45 +0100

    Update libft.h

--- inc/libft.h
+++ inc/libft.h
@@ -54,7 +54,6 @@ char	*ft_strdup(const char *src);
 //fonctions absentes/differentes de la libc
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-int	is_in_set(char const *set, char c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const*s, char c);
 char	*ft_itoa(int n);
@@ -80,4 +79,7 @@ ssize_t	end_of_line(char *s);
 char	*update_stash(char *stash, int eol);
 ssize_t	strchr_(char *s, char c);
 ssize_t	ft_strlen_(const char *str);
+
+//persos
+int  is_in_set(char const *set, char c);
 #endif
